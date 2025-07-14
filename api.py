1cfca544df6d1987a9e83fd4ad79b2b96076f113
S1erben13 <asimovchannel@gmail.com>
2025-07-14 04:04:29 +0300

    new endpoint for filling db

--- api.py
+++ api.py
@@ -1,6 +1,10 @@
 import logging
+import os
 from typing import Optional
-from fastapi import Depends, FastAPI, HTTPException, Query
+from fastapi import Depends, FastAPI, HTTPException, Query, Request, Body
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 from database import db_config, get_db_connection, get_products
 
@@ -31,7 +35,6 @@ async def read_products(
         exact_feedbacks: Optional[int] = Query(
             None, description="Exact number of feedbacks", ge=0
         ),
-        conn=Depends(get_db_connection),
 ):
     """
     Get list of products with advanced filtering options:
@@ -48,27 +51,54 @@ async def read_products(
         * max_feedbacks: Maximum number of feedbacks
         * exact_feedbacks: Exact number of feedbacks
     """
-    try:
-        logger.info(
-            f"Fetching products with filters: "
-            f"min_price={min_price}, max_price={max_price}, exact_price={exact_price}, "
-            f"min_rating={min_rating}, max_rating={max_rating}, exact_rating={exact_rating}, "
-            f"min_feedbacks={min_feedbacks}, max_feedbacks={max_feedbacks}, exact_feedbacks={exact_feedbacks}"
-        )
 
-        products = get_products(
-            conn,
-            min_price=min_price,
-            max_price=max_price,
-            exact_price=exact_price,
-            min_rating=min_rating,
-            max_rating=max_rating,
-            exact_rating=exact_rating,
-            min_feedbacks=min_feedbacks,
-            max_feedbacks=max_feedbacks,
-            exact_feedbacks=exact_feedbacks,
-        )
-        return {"products": products}
+    try:
+        with get_db_connection() as conn:
+            logger.info(
+                f"Fetching products with filters: "
+                f"min_price={min_price}, max_price={max_price}, exact_price={exact_price}, "
+                f"min_rating={min_rating}, max_rating={max_rating}, exact_rating={exact_rating}, "
+                f"min_feedbacks={min_feedbacks}, max_feedbacks={max_feedbacks}, exact_feedbacks={exact_feedbacks}"
+            )
+
+            products = get_products(
+                conn,
+                min_price=min_price,
+                max_price=max_price,
+                exact_price=exact_price,
+                min_rating=min_rating,
+                max_rating=max_rating,
+                exact_rating=exact_rating,
+                min_feedbacks=min_feedbacks,
+                max_feedbacks=max_feedbacks,
+                exact_feedbacks=exact_feedbacks,
+            )
+            return {"products": products}
     except Exception as e:
         logger.error(f"Error fetching products: {str(e)}")
         raise HTTPException(status_code=500, detail="Internal server error")
+
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+@app.get("/", response_class=HTMLResponse)
+async def read_item(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.post("/api/products/")
+async def fill_db(
+        body_query: Optional[str] = Body(None, alias="query",
+                                         description="Searching query Wildberries"),
+        quantity: Optional[int] = Body(None, alias="quantity",
+                                         description="Quantity of products")
+):
+    search_query = body_query
+    logging.info(f'API search_query: "{search_query}"')
+    logging.info(f'API quantity: "{quantity}"')
+    if not search_query:
+        return {"error": "Query parameter is required"}
+
+    command = f"python manage.py search {quantity} {search_query}"
+    os.system(command)
+
+    return {"query": "success"}
